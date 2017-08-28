b8dc667352b5aafafad123559d804c48dfd01d40
ShangyinGao <shangyingao@gmail.com>
2017-08-28 19:29:42 +0200

    exercise 2

--- Exercise2/exercise2.m
+++ Exercise2/exercise2.m
@@ -1,25 +1,51 @@
 % Assignment 2 Exercise 2 hidden Markov model
 % Date: 2017-08-27
 % Author: ShangyinGao
-clear;
-close all;
-clc;
 
 %% load data
 A = importdata('A.txt');
 B = importdata('B.txt');
 pi = importdata('pi.txt');
 test = importdata('Test.txt');
+% class memory
+class = zeros(1, 10);
+loglikelihood = 0;
 
-% nth repetition 1:10
-n = 1;
+%% nth repetition 1:10
+for n = 1:10;
 
 %% forward procedure
-alpha = zeros(12,12);
+% 
+alpha = zeros(12,60);
 alphaInit = zeros(12,1);
+
+% calculate alphaInit
 for i = 1:12
     alphaInit(i) = pi(i)*B(test(1, n),i);
 end
+alpha(:, 1) = alphaInit;
 
+% calculate alpha
+sumAlpha = zeros(12,1);
+for i = 2:60
+    for j = 1:12
+        for k = 1:12
+            sumAlpha(k) = alpha(k, i-1).*A(k,j);
+        end    
+        alpha(j, i) = sum(sumAlpha).*B(test(i, n), j);
+        
+    end
+    
+end
 
-% i = 1 -> alpha1
+%% log likelihood
+loglikelihood = log(sum(alpha(:, 60)));
+if loglikelihood > -120
+    class(n) = 1;
+else
+    class(n) = 2;
+end
+
+end
+
+class
